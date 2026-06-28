33988dcbeb7341018735cb5c02631292e98ffea3
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-06-28 19:57:44 +0200

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,1 +1,9 @@
+CREATE TABLE form2 (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100) NOT NULL,
+  subject VARCHAR(100) NOT NULL,
+  age INTEGER CHECK (age >= 18),
+  message VARCHAR(500) NOT NULL
+);
 
