9de25654801c7c543f1f04996ad185759f26091f
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-06-28 20:17:48 +0200

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,1 +1,7 @@
-
+INSERT INTO form2 (name, email, subject, age, message)
+VALUES 
+('Ana', 'ana@gmail.com', 'information', NULL, 'hola testing'),
+(NULL, 'pedro@gmail.com', 'job', 20, 'Pedro mensaje' ),
+('María', 'maria@yahoo.com', 'other', 25, 'mensaje de prueba'),
+('Ana', 'ana.work@gmail.com', 'job', 30, 'test message'),
+(NULL, 'user1@gmail.com', 'information', 20, 'information mensaje testing');
